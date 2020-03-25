cb48e25f8b7c8c1224174225b208b9dad27f50b1
michaldobiezynski <dobiezynski@gmail.com>
2020-03-25 13:40:27 +0000

    verifying blockchain

--- blockchain.py
+++ blockchain.py
@@ -37,10 +37,27 @@ def print_blockchain_elements():
         print(block)
 
 
+def verify_chain():
+    block_index = 0
+    is_valid = True
+    for block in blockchain:
+        if block_index == 0:
+            block_index += 1
+            continue
+        if block[0] == blockchain[block_index - 1]:
+            is_valid = True
+        else:
+            is_valid = False
+            break
+        block_index += 1
+    return is_valid
+
+
 while True:
     print('Please choose')
     print('1: Add a new transaction value')
     print('2: Output the blockchain blocks')
+    print('h: Manipulate the chain')
     print('q: Quit')
     user_choice = get_user_choice()
 
@@ -51,9 +68,15 @@ while True:
         print_blockchain_elements()
     elif user_choice == 'q':
         break
+    elif user_choice == 'h':
+        if len(blockchain) >= 1:
+            blockchain[0] = [2]
     else:
         print('Input was invalid, please pick a value from the list!')
     print('Choice registered!')
+    if not verify_chain():
+        print('Invalid blockchain!')
+        break
 
 
 print('Done!')
