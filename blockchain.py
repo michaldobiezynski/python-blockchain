8d5ef48e7f076dfd82a07b729f92d7bcaa5f1c3d
michaldobiezynski <dobiezynski@gmail.com>
2020-03-26 07:31:46 +0000

    hashing previous blocks

--- blockchain.py
+++ blockchain.py
@@ -30,11 +30,13 @@ def add_transaction(recipient, sender=owner, amount=1.0):
 
 def mine_block():
     last_block = blockchain[-1]
-    hashed_block = ''
+    hashed_block = str([last_block[key] for key in last_block])
+    print(hashed_block)
+    # hashed_block = ''
 
-    for key in last_block:
-        value = last_block[key]
-        hashed_block = hashed_block + str(value)
+    # for key in last_block:
+    #     value = last_block[key]
+    #     hashed_block = hashed_block + str(value)
 
     block = {
         'previous_hash': hashed_block,
