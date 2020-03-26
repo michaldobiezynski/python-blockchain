8a05035d7576a0392aba242496536f111cf017a8
michaldobiezynski <dobiezynski@gmail.com>
2020-03-26 15:27:24 +0000

    verify all transactions

--- blockchain.py
+++ blockchain.py
@@ -123,6 +123,10 @@ def verify_chain():
     return True
 
 
+def verify_transactions():
+    return all([verify_transaction(tx) for tx in open_transaction])
+
+
 waiting_for_input = True
 
 while waiting_for_input:
@@ -131,6 +135,7 @@ while waiting_for_input:
     print('2: Mine a new block')
     print('3: Output the blockchain blocks')
     print('4: Output the participants')
+    print('5: Check transaction validity')
     print('h: Manipulate the chain')
     print('q: Quit')
     user_choice = get_user_choice()
@@ -153,6 +158,11 @@ while waiting_for_input:
         print_blockchain_elements()
     elif user_choice == '4':
         print(participants)
+    elif user_choice == '5':
+        if verify_transactions():
+            print('All transactions are valid')
+        else:
+            print('There are invalid transactions')
     elif user_choice == 'q':
         waiting_for_input = False
     elif user_choice == 'h':
